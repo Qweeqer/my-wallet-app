77c9e6947711c215c67955baeb749dff8e634d86
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-07-24 23:19:33 +0300

    add logo component

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,14 @@
+import "./Logo.module.css";
+
+function Logo() {
+  return (
+    <img
+      className="logo"
+      src="./logo.jpg"
+      alt="Logo"
+      style={{ width: "60px", height: "100%" }}
+    />
+  );
+}
+
+export default Logo;
