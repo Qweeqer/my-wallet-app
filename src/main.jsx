4d1edfb7f14eb5bb1275b6635007e91c6ca58f10
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-07-24 23:23:57 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.js";
+import App from "./App.jsx";
+import "./index.css";
+import { Loader } from "./components/Loader/Loader.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
