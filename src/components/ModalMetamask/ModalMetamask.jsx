fc34a0ad1f109cfadfe16a2889988416d0320fac
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-07-26 15:38:12 +0300

    change logic for mobile

--- src/components/ModalMetamask/ModalMetamask.jsx
+++ src/components/ModalMetamask/ModalMetamask.jsx
@@ -6,18 +6,54 @@ function ModalMetamask({ isModalOpen, setIsModalOpen }) {
 
   let link;
   let linkText;
+  let modalContent;
 
-  if (/android/i.test(userAgent)) {
-    link = "https://play.google.com/store/apps/details?id=io.metamask";
-    linkText = "MetaMask for Android";
-  } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    link =
-      "https://apps.apple.com/us/app/metamask-blockchain-wallet/id1438144202";
-    linkText = "MetaMask for IOS";
+  if (
+    /android/i.test(userAgent) ||
+    (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)
+  ) {
+    if (window.ethereum) {
+      modalContent = "MetaMask is installed. Please connect to it.";
+    } else {
+      link = /android/i.test(userAgent)
+        ? "https://play.google.com/store/apps/details?id=io.metamask"
+        : "https://apps.apple.com/us/app/metamask-blockchain-wallet/id1438144202";
+      linkText = /android/i.test(userAgent)
+        ? "MetaMask for Android"
+        : "MetaMask for IOS";
+      modalContent = (
+        <>
+          For use thise APP, please, install{" "}
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="modal-link"
+          >
+            {linkText}
+          </a>
+          .
+        </>
+      );
+    }
   } else {
     link =
       "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en";
     linkText = "MetaMask for PC";
+    modalContent = (
+      <>
+        For use this APP, please, install{" "}
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="modal-link"
+        >
+          {linkText}
+        </a>
+        .
+      </>
+    );
   }
 
   return (
@@ -34,18 +70,7 @@ function ModalMetamask({ isModalOpen, setIsModalOpen }) {
         Close
       </button>
       <h2 className="modal-title">You must install MetaMask</h2>
-      <p className="modal-content">
-        For use thise APP, please, install{" "}
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="modal-link"
-        >
-          {linkText}
-        </a>
-        .
-      </p>
+      <p className="modal-content">{modalContent}</p>
     </Modal>
   );
 }
