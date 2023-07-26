427747fb3421d73454591f8bf2479ca2fd8cea14
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-07-26 15:21:22 +0300

    change view  wallet address and hover effect view  wallet address

--- src/components/ConnectWalletButton/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton/ConnectWalletButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 function ConnectWalletButton({
@@ -6,10 +7,25 @@ function ConnectWalletButton({
   balance,
   connectWallet,
 }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const truncatedWalletAddress =
+    walletAddress &&
+    `${walletAddress.slice(0, 5)}...${walletAddress.slice(-4)}`;
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   return isWalletConnected ? (
-    <div className="connectWallet">
+    <div
+      className="connectWallet"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <span className="walletBalance">{balance} ETH</span>
-      <span className="walletAddress">{walletAddress}</span>
+      <span className="walletAddress">
+        {isHovered ? walletAddress : truncatedWalletAddress}
+      </span>
     </div>
   ) : (
     <button className="connectButton" onClick={connectWallet}>
